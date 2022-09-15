6a2239a1fb170e50d77cce5eccecb96251728e5a
RobertYam7 <robert8y@gmail.com>
2022-09-15 09:07:43 -0500

    added integration and unit tests

---
+++ spec/feature/integration_spec.rb
@@ -0,0 +1,15 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'To Kill a Mockingbird'
+    fill_in 'Author', with: 'Harper Lee'
+    fill_in 'Price', with: '45'
+    fill_in 'Published', with: '9/15/2022'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('To Kill a Mockingbird')
+  end
+end
