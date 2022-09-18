2d05e1808ba80718d881c4d5744b63cb34b1a6e1
RobertYam7 <robert8y@gmail.com>
2022-09-18 13:33:24 -0500

    added unit and integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,50 +1,20 @@
-# location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
+# Example
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'To Kill a Mockingbird'
-    fill_in 'Author', with: 'Harper Lee'
-    fill_in 'Price', with: '45'
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 19.99
+    select '2020' :from => 'book_published_date_1i'
+    select '9' :from => 'book_published_date_2i'
+    select '28' :from => 'book_published_date_3i'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('To Kill a Mockingbird')
-  end
-end
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Title', with: 'To Kill a Mockingbird'
-    fill_in 'Author', with: 'Harper Lee'
-    fill_in 'Price', with: '45'
-    fill_in 'Published', with: '7/11'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('To Kill a Mockingbird')
-  end
-end
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Title', with: 'To Kill a Mockingbird'
-    fill_in 'Author', with: 'Harper Lee'
-    fill_in 'Price', with: '45'
-    fill_in 'Published', with: '7/11'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('To Kill a Mockingbird')
-  end
-end
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Title', with: 'To Kill a Mockingbird'
-    fill_in 'Author', with: 'Harper Lee'
-    fill_in 'Price', with: '45'
-    fill_in 'Published', with: '7/11'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('To Kill a Mockingbird')
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('JK Rowling')
+    expect(page).to have_content('19.99')
+    expect(page).to have_content('2020-09-28')
   end
 end
